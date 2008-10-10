2d23f74cb4111ba42c5875d11990ffb5f9554dbe
David Chelimsky <dchelimsky@gmail.com>
2008-10-10 09:46:29 -0500

    remove doc (old webby site, replaced by website)

--- doc/docspec.rb
+++
@@ -1,6 +0,0 @@
-# Helper for webgen doco that works on both windows and posix
-# cd ../example_rails_app/vendor/plugins/rspec && ruby bin/spec
-Dir.chdir(File.dirname(__FILE__) + '/../example_rails_app/vendor/plugins/rspec') do
-  puts `ruby bin/spec #{ARGV.join(" ")}`
-end
-nil
