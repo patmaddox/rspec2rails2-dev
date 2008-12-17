b0e2bf2e2508c007a236f91066da63cd537e1e10
David Chelimsky <dchelimsky@gmail.com>
2008-12-17 11:01:26 -0600

    add session_key to env

--- example_rails_app/config/environment.rb
+++ example_rails_app/config/environment.rb
@@ -2,6 +2,12 @@ require File.join(File.dirname(__FILE__), 'boot')
 require "rubygems"
 
 Rails::Initializer.run do |config|
+  if config.action_controller.respond_to?(:session=)
+    config.action_controller.session = {
+      :session_key => '_rspec_rails_session',
+      :secret      => '5716a6ba4342c76063b4a65dd7395e60b19c9d87f4bcc88af4322af953bf4e334e615f4810a62f1e13d241b4234c967ae8de7501282fd4234cfc5d49457b25cf'
+    }
+  end
 end
 
 def in_memory_database?
