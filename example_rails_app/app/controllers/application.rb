24a0e65d7b86d24032df9855262af104d3035e22
David Chelimsky <dchelimsky@gmail.com>
2008-12-23 22:51:07 -0500

    move sample before filters from example app to plugin specs

--- example_rails_app/app/controllers/application.rb
+++ example_rails_app/app/controllers/application.rb
@@ -1,11 +1,4 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
-  before_filter :i_should_only_be_run_once, 
-                :only => 'action_with_inherited_before_filter'
-  
-  def i_should_only_be_run_once
-    true
-  end
-  private :i_should_only_be_run_once
 end
