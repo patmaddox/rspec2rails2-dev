9845bbe87b1c0df74e69ac373b2704025bc9c62a
David Chelimsky <dchelimsky@gmail.com>
2008-12-23 23:04:16 -0500

    move models for rspec-rails specs to plugin

--- example_rails_app/app/models/survey.rb
+++
@@ -1,1 +0,0 @@
-class Survey; end
--- example_rails_app/config/routes.rb
+++ example_rails_app/config/routes.rb
@@ -1,7 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :surveys do |survey|
-    survey.resource :report, :controller =>'surveys/report'
-  end
-
   map.connect ':controller/:action/:id'
 end
