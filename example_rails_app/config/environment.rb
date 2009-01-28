d63cefe0c609f1df84504fb2f9d51cfd11a9e93a
David Chelimsky <dchelimsky@gmail.com>
2009-01-28 02:15:56 -0600

    unnecessary conditional

--- example_rails_app/config/environment.rb
+++ example_rails_app/config/environment.rb
@@ -2,12 +2,10 @@ require File.join(File.dirname(__FILE__), 'boot')
 require "rubygems"
 
 Rails::Initializer.run do |config|
-  if Rails.const_defined?(:VERSION) && Rails::VERSION::STRING >= '1.2.7'
-    config.action_controller.session = {
-      :session_key => '_rspec_rails_session',
-      :secret      => '5716a6ba4342c76063b4a65dd7395e60b19c9d87f4bcc88af4322af953bf4e334e615f4810a62f1e13d241b4234c967ae8de7501282fd4234cfc5d49457b25cf'
-    }
-  end
+  config.action_controller.session = {
+    :session_key => '_rspec_rails_session',
+    :secret      => '5716a6ba4342c76063b4a65dd7395e60b19c9d87f4bcc88af4322af953bf4e334e615f4810a62f1e13d241b4234c967ae8de7501282fd4234cfc5d49457b25cf'
+  }
 end
 
 def in_memory_database?
