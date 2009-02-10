ff8551f41d4b2c40b554f199d71b67f57198f2fc
David Chelimsky <dchelimsky@gmail.com>
2009-02-10 04:21:02 -0600

    run against rails 2.3.0 by default

--- example_rails_app/config/boot.rb
+++ example_rails_app/config/boot.rb
@@ -18,7 +18,7 @@ module Rails
   end
 end
 
-ENV['RSPEC_RAILS_VERSION'] ||= "2.2.2"
+ENV['RSPEC_RAILS_VERSION'] ||= "2.3.0"
 puts "running against rails #{ENV['RSPEC_RAILS_VERSION']}"
 
 unless defined?(Rails::Initializer)
